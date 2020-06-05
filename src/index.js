9f54d3c1ef99286a71e44013f93ebc5a4e27d5a0
Daniel Nichols <herrnichols@gmail.com>
2020-06-05 16:14:27 -0400

    styles working in webpack

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import $ from 'jquery';
 import list from './list';
 import api from './api';
 import store from './store';
+import './styles.css';
 
 function main() {
   list.eventListeners();
   list.render();
-
-  api.getBookmarks().then(items => {
-    items.forEach(item => store.addBookmark(item));
+  api.getBookmarks().then((items) => {
+    items.forEach((item) => store.addBookmark(item));
     list.render();
   });
 }
